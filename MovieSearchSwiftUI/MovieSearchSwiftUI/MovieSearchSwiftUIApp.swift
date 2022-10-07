519c9f6f8d13b3d9f575f456efdbc5cf47d653ac
Jordy De Jonghe <jordy.dejonghe@icapps.com>
2022-10-07 09:40:11 +0200

    init

---
+++ MovieSearchSwiftUI/MovieSearchSwiftUI/MovieSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchSwiftUIApp.swift
+//  MovieSearchSwiftUI
+//
+//  Created by Jordy De Jonghe on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
